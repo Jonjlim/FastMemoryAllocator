47db62a3bd2ad68fe89786989e2254e530c3b4e4
Jonathon Lim <limjonathon321@gmail.com>
2026-06-19 15:40:20 -0700

    Update

--- src/cmalloc.c
+++ src/cmalloc.c
@@ -15,17 +15,17 @@ static span_t *bins[SIZE_CLASS_COUNT];
 
 static inline void remove_span_from_bin(span_t *target, span_t **head) {
     if (*head == target) {
-        *head = target->next_in_bin;
+        *head = target->next_in_size_class_bin;
     } else {
         span_t *prev = *head;
-        while (prev != NULL && prev->next_in_bin != target) {
-            prev = prev->next_in_bin;
+        while (prev != NULL && prev->next_in_size_class_bin != target) {
+            prev = prev->next_in_size_class_bin;
         }
         if (prev != NULL) {
-            prev->next_in_bin = target->next_in_bin;
+            prev->next_in_size_class_bin = target->next_in_size_class_bin;
         }
     }
-    target->next_in_bin = NULL;
+    target->next_in_size_class_bin = NULL;
 }
 
 void *cmalloc(size_t size) {
@@ -34,13 +34,13 @@ void *cmalloc(size_t size) {
         if (bins[size_class_index]) {
             span_t *span = bins[size_class_index];
             if (span->free_count == 1) {
-                bins[size_class_index] = span->next_in_bin;
-                span->next_in_bin = NULL;
+                bins[size_class_index] = span->next_in_size_class_bin;
+                span->next_in_size_class_bin = NULL;
             }
             return allocate_block(span);
         } else {
             span_t *span = cmalloc_initialize_span(size_class_index, size);
-            span->next_in_bin = NULL;
+            span->next_in_size_class_bin = NULL;
             bins[size_class_index] = span;
             return allocate_block(span);
         }
@@ -54,16 +54,16 @@ void cfree(void *ptr) {
     if (ptr == NULL) return;
     span_t *span = cmalloc_get_span(ptr);
     if (span->size_class_index == LARGE_CLASS_SIZE_INDEX) {
-        cmalloc_uninitialize_span(span);
+        cmalloc_cache_span(span);
     } else {
         if (span->free_count == 0) {
-            span->next_in_bin = bins[span->size_class_index];
+            span->next_in_size_class_bin = bins[span->size_class_index];
             bins[span->size_class_index] = span;
         }
         free_block(ptr, span);
         if (span->free_count == span->block_count) {
             remove_span_from_bin(span, &bins[span->size_class_index]);
-            cmalloc_uninitialize_span(span);
+            cmalloc_cache_span(span);
         }
     }
 }
--- src/span.c
+++ src/span.c
@@ -26,40 +26,33 @@ typedef struct data_chunk_struct {
     struct data_chunk_struct *prev;
 } data_chunk_t;
 
-typedef struct page_sequence_struct {
-    char *base;
-    size_t page_count;
-    struct page_sequence_struct *phys_next;
-    struct page_sequence_struct *phys_prev;
-    struct page_sequence_struct *free_next;
-    char is_initialized;
-} page_sequence_t;
-
 static range_map_t *page_map = NULL;
-static range_map_t *page_sequence_map;
 static data_chunk_t *data_arena;
-static page_sequence_t *binned_page_sequences[MAX_BINNED_PAGES];
-static uint64_t non_empty_biinned_page_sequences_bitmap[FREE_BITMAP_WORDS];
+static span_t *uninitilized_span_bin[MAX_BINNED_PAGES];
+static uint64_t non_empty_bitmap[FREE_BITMAP_WORDS];
 
-static inline void mark_free_page_sequence_non_empty(size_t page_count) {
+static inline void mark_binned_span_non_empty(size_t page_count) {
     size_t word_index = PAGE_BITMAP_WORD_INDEX(page_count);
     size_t bit_index  = PAGE_BITMAP_BIT_INDEX(page_count);
-    non_empty_biinned_page_sequences_bitmap[word_index] |= (UINT64_C(1) << bit_index);
+    non_empty_bitmap[word_index] |= (UINT64_C(1) << bit_index);
 }
 
-static inline void mark_free_page_sequence_empty(size_t page_count) {
+static inline void mark_binned_span_empty(size_t page_count) {
     size_t word_index = PAGE_BITMAP_WORD_INDEX(page_count);
     size_t bit_index  = PAGE_BITMAP_BIT_INDEX(page_count);
 
-    non_empty_biinned_page_sequences_bitmap[word_index] &= ~(UINT64_C(1) << bit_index);
+    non_empty_bitmap[word_index] &= ~(UINT64_C(1) << bit_index);
 }
 
-static inline size_t find_closest_fitting_free_page_sequence(size_t page_count) {
+/**
+ * @brief Returns the page count of the closest fitting uninitialized span.
+ */
+static inline size_t find_best_fitting_uninitialized_span(size_t page_count) {
     size_t word_index = PAGE_BITMAP_WORD_INDEX(page_count);
     size_t bit_index  = PAGE_BITMAP_BIT_INDEX(page_count);
 
     uint64_t word =
-        non_empty_biinned_page_sequences_bitmap[word_index] &
+        non_empty_bitmap[word_index] &
         (UINT64_MAX << bit_index);
 
     while (1) {
@@ -80,53 +73,60 @@ static inline size_t find_closest_fitting_free_page_sequence(size_t page_count) 
             return NO_FITTING_PAGE_SEQUENCE;
         }
 
-        word = non_empty_biinned_page_sequences_bitmap[word_index];
+        word = non_empty_bitmap[word_index];
     }
 }
 
-void *cmalloc_alloc_data(size_t size) {
+static inline span_t *alloc_span(size_t size) {
+    if (page_map == NULL) page_map = cmalloc_initialize_range_map();
+
     size_t page_count = round_up_page(size) >> PAGE_SHIFT;
 
-    if (page_count >= MAX_BINNED_PAGES) return mmap(NULL,
-        round_up_page(size),
-        PROT_READ | PROT_WRITE,
-        MAP_PRIVATE | MAP_ANONYMOUS,
-        -1,
-        0);
+    if (page_count >= MAX_BINNED_PAGES) {
+        span_t *span = cmalloc_alloc_metadata(sizeof(span_t));
+        span->base = mmap(NULL,
+            page_count << PAGE_SHIFT,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS,
+            -1,
+            0);
+        span->page_count = page_count;
+        span->is_initialized = 0;
+        cmalloc_map_range(page_map, span, round_down_page_index(span->base),
+            round_down_page_index(span->base) + span->page_count);
+        return span;
+    }
 
-    if (page_sequence_map == NULL) page_sequence_map = cmalloc_initialize_range_map();
-
-    size_t best_fitting_pages = find_closest_fitting_free_page_sequence(page_count);
+    size_t best_fitting_pages = find_best_fitting_uninitialized_span(page_count);
     if (data_arena && best_fitting_pages != NO_FITTING_PAGE_SEQUENCE) {
         if (page_count == best_fitting_pages) {
-            page_sequence_t *temp = binned_page_sequences[page_count];
-            binned_page_sequences[page_count] = temp->free_next;
-            if (binned_page_sequences[page_count] == NULL)
-                mark_free_page_sequence_empty(page_count);
-            temp->is_initialized = 0;
-            return temp->base;
+            span_t *temp = uninitilized_span_bin[page_count];
+            uninitilized_span_bin[page_count] = temp->next_in_uninitialized_bin;
+            if (uninitilized_span_bin[page_count] == NULL)
+                mark_binned_span_empty(page_count);
+            return temp;
         } else {
-            page_sequence_t *best_fit = binned_page_sequences[best_fitting_pages];
-            binned_page_sequences[best_fitting_pages] = best_fit->free_next;
-            if (binned_page_sequences[best_fitting_pages] == NULL)
-                mark_free_page_sequence_empty(best_fitting_pages);
-            best_fit->is_initialized = 0;
+            span_t *best_fit = uninitilized_span_bin[best_fitting_pages];
+            uninitilized_span_bin[best_fitting_pages] = best_fit->next_in_uninitialized_bin;
+            if (uninitilized_span_bin[best_fitting_pages] == NULL)
+                mark_binned_span_empty(best_fitting_pages);
             best_fit->page_count = page_count;
 
-            page_sequence_t *split = cmalloc_alloc_metadata(sizeof(page_sequence_t));
+            span_t *split = cmalloc_alloc_metadata(sizeof(span_t));
             split->base = best_fit->base + (page_count << PAGE_SHIFT);
-            cmalloc_map(page_sequence_map, split, round_down_page_index(split->base));
             split->page_count = best_fitting_pages - page_count;
-            split->is_initialized = 1;
+            split->is_initialized = 0;
             split->phys_next = best_fit->phys_next;
             split->phys_prev = best_fit;
-            split->free_next = binned_page_sequences[best_fitting_pages - page_count];
-            binned_page_sequences[best_fitting_pages - page_count] = split;
-            mark_free_page_sequence_non_empty(best_fitting_pages - page_count);
+            split->next_in_uninitialized_bin = uninitilized_span_bin[best_fitting_pages - page_count];
+            uninitilized_span_bin[best_fitting_pages - page_count] = split;
+            cmalloc_map_range(page_map, split, round_down_page_index(split->base),
+                round_down_page_index(split->base) + split->page_count);
+            mark_binned_span_non_empty(best_fitting_pages - page_count);
 
             if (best_fit->phys_next) best_fit->phys_next->phys_prev = split;
             best_fit->phys_next = split;
-            return best_fit->base;
+            return best_fit;
         }
     } else {
         data_chunk_t *prev = data_arena;
@@ -141,83 +141,89 @@ void *cmalloc_alloc_data(size_t size) {
         if (data_arena->base == MAP_FAILED) {
             return NULL;
         }
-        page_sequence_t *r = cmalloc_alloc_metadata(sizeof(page_sequence_t));
-        r->base = data_arena->base;
-        cmalloc_map(page_sequence_map, r, round_down_page_index(r->base));
-        r->free_next = NULL;
-        r->page_count = page_count;
-        r->is_initialized = 0;
-        binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count] = cmalloc_alloc_metadata(sizeof(page_sequence_t));
-        binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count]->base = data_arena->base + (page_count << PAGE_SHIFT);
-        cmalloc_map(page_sequence_map, binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count],
-            round_down_page_index(binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count]->base));
-        binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count]->free_next = NULL;
-        binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count]->page_count = DATA_CHUNK_PAGE_COUNT - page_count;
-        r->phys_prev = NULL;
-        r->phys_next = binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count];
-        binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count]->phys_prev = r;
-        binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count]->phys_next = NULL;
-        binned_page_sequences[DATA_CHUNK_PAGE_COUNT - page_count]->is_initialized = 1;
-        mark_free_page_sequence_non_empty(DATA_CHUNK_PAGE_COUNT - page_count);
-        return r->base;
+        span_t *fit = cmalloc_alloc_metadata(sizeof(span_t));
+        fit->base = data_arena->base;
+        fit->next_in_uninitialized_bin = NULL;
+        fit->page_count = page_count;
+        fit->is_initialized = 0;
+        cmalloc_map_range(page_map, fit, round_down_page_index(fit->base),
+            round_down_page_index(fit->base) + fit->page_count);
+
+        span_t *split = cmalloc_alloc_metadata(sizeof(span_t));
+        split->base = data_arena->base + (page_count << PAGE_SHIFT);
+        split->next_in_uninitialized_bin = NULL;
+        split->page_count = DATA_CHUNK_PAGE_COUNT - page_count;
+        split->is_initialized = 0;
+        cmalloc_map_range(page_map, split, round_down_page_index(split->base),
+            round_down_page_index(split->base) + split->page_count);
+
+        fit->phys_prev = NULL;
+        fit->phys_next = split;
+        split->phys_prev = fit;
+        split->phys_next = NULL;
+        mark_binned_span_non_empty(DATA_CHUNK_PAGE_COUNT - page_count);
+        uninitilized_span_bin[DATA_CHUNK_PAGE_COUNT - page_count] = split;
+        return fit;
     }
 }
-void cmalloc_free_data(void *ptr, size_t size) {
-    size_t page_count = round_up_page(size) >> PAGE_SHIFT;
 
-    if (page_count >= MAX_BINNED_PAGES) {
-        munmap(ptr, round_up_page(size));
+void cmalloc_cache_span(span_t *span) {
+    if (span->page_count >= MAX_BINNED_PAGES) {
+        cmalloc_unmap_range(page_map, round_down_page_index(span->base),
+            round_down_page_index(span->base) + span->page_count);
+        munmap(span->base, span->page_count << PAGE_SHIFT);
+        cmalloc_free_metadata(span, sizeof(span_t));
         return;
     }
 
-    page_sequence_t *ps = cmalloc_get(page_sequence_map, round_down_page_index(ptr));
-    ps->is_initialized = 1;
-    ps->free_next = binned_page_sequences[page_count];
-    binned_page_sequences[page_count] = ps;
-    mark_free_page_sequence_non_empty(page_count);
+    span->is_initialized = 0;
+    span->next_in_uninitialized_bin = uninitilized_span_bin[span->page_count];
+    uninitilized_span_bin[span->page_count] = span;
+    mark_binned_span_non_empty(span->page_count);
 }
 
 
 span_t *cmalloc_initialize_span(int size_class_index, size_t requested_size) {
-    if (page_map == NULL) page_map = cmalloc_initialize_range_map();
-    assert(page_map);
-
     span_t *new = NULL;
     if (size_class_index == LARGE_CLASS_SIZE_INDEX) {
-        new = (span_t *)cmalloc_alloc_metadata(get_span_md_size(1));
-        new->span_size = round_up_page(requested_size);
+        new = alloc_span(requested_size);
+        assert(new);
         new->block_size = requested_size;
         new->block_count = 1;
     } else {
-        new = (span_t *)cmalloc_alloc_metadata(get_span_md_size(SIZE_CLASS_BLOCK_COUNT[size_class_index]));
-        new->span_size = round_up_page(SIZE_CLASS_SPAN_SIZE[size_class_index]);
+        new = alloc_span(SIZE_CLASS_SPAN_SIZE[size_class_index]);
+        assert(new);
         new->block_size = SIZE_CLASSES[size_class_index];
         new->block_count = SIZE_CLASS_BLOCK_COUNT[size_class_index]; 
     }
+    new->is_initialized = 1;
     new->size_class_index = size_class_index;
-    new->data_address = cmalloc_alloc_data(new->span_size);
     new->free_count = new->block_count;
-    cmalloc_map_range(page_map, new, round_down_page_index(new->data_address),
-        round_down_page_index(((char *) new->data_address) + new->span_size));
 
-    new->block_bitmap = (uint64_t *) ((char *) new + sizeof(span_t));
-    size_t count = (new->block_count + (size_t) 63) & ~63;
-    size_t chunk_count = count >> 6;
-    assert(chunk_count <= 64);
-    if (chunk_count == 64) new->nonfull_bitmap = UINT64_MAX;
-    else  new->nonfull_bitmap = (1ULL << chunk_count) - 1ULL;
-    if (count > new->block_count) new->block_bitmap[chunk_count - 1] = (1ULL << (new->block_count % 64)) - 1ULL;
-    else new->block_bitmap[chunk_count - 1] = UINT64_MAX;
-    for (size_t i = 0; i < chunk_count - 1; i++) new->block_bitmap[i] = UINT64_MAX;
+    size_t word_count =
+        (new->block_count + (SPAN_BLOCK_BITMAP_BITS_PER_WORD - 1))
+        / SPAN_BLOCK_BITMAP_BITS_PER_WORD;
+    size_t blocks_in_last_word =
+        new->block_count
+        - ((word_count - 1) * SPAN_BLOCK_BITMAP_BITS_PER_WORD);
+
+    new->nonfull_bitmap = 0;
+    for (size_t i = 0; i < SPAN_BLOCK_BITMAP_WORD_COUNT; i++)
+        new->block_bitmap[i] = 0;
+
+    for (size_t word_index = 0; word_index < word_count; word_index++) {
+        uint64_t free_blocks;
+        if (word_index == word_count - 1
+            && blocks_in_last_word < SPAN_BLOCK_BITMAP_BITS_PER_WORD) {
+            free_blocks = (UINT64_C(1) << blocks_in_last_word) - UINT64_C(1);
+        } else {
+            free_blocks = UINT64_MAX;
+        }
+        new->block_bitmap[word_index] = free_blocks;
+        new->nonfull_bitmap |= SPAN_BLOCK_BITMAP_BIT(word_index);
+    }
 
     return new;
-}
-
-void cmalloc_uninitialize_span(span_t *span) {
-    cmalloc_unmap_range(page_map, round_down_page_index(span->data_address),
-        round_down_page_index(((char *) span->data_address) + span->span_size));
-    cmalloc_free_data(span->data_address, span->span_size);
-    cmalloc_free_metadata(span, get_span_md_size(span->block_count));
 }
 
 span_t *cmalloc_get_span(void *ptr) {
